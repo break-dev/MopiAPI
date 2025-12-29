683f0c3414c0fe057c322d054f11617e26801db3
Break <franklinbacacampos@gmail.com>
2025-12-28 21:00:46 -0500

    feat: Implement rotating file handler for logging and use a shared formatter.

--- app/core/logger.py
+++ app/core/logger.py
@@ -1,5 +1,5 @@
 import logging
-
+from logging.handlers import RotatingFileHandler
 #
 from core.settings import settings
 
@@ -9,16 +9,18 @@ logger.setLevel(logging.INFO)
 
 # Handlers: Stream + File
 if not logger.handlers:
+    # Formateador compartido
+    formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
 
+    # Console (Stream)
     stream_h = logging.StreamHandler()
-    stream_h.setFormatter(
-        logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
-    )
+    stream_h.setFormatter(formatter)
     logger.addHandler(stream_h)
 
-    # File handler opcional
-    file_h = logging.FileHandler(settings.LOG_FILE_PATH)
-    file_h.setFormatter(
-        logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+    # File con Rotación: 4MB por archivo, conserva el ultimo
+    # 4MB = 4194304 bytes
+    file_h = RotatingFileHandler(
+        settings.LOG_FILE_PATH, maxBytes=4194304, backupCount=1
     )
+    file_h.setFormatter(formatter)
     logger.addHandler(file_h)
