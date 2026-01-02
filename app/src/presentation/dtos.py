129347ca0964a61d7a49d405154928a12fd61cc7
Break <franklinbacacampos@gmail.com>
2026-01-01 21:50:45 -0500

    simplificacion de dtos

--- app/src/presentation/dtos.py
+++ app/src/presentation/dtos.py
@@ -2,33 +2,24 @@ from pydantic import BaseModel, Field
 from typing import Optional
 
 #
-from src.domain.enums import (
-    AudioPlatforms,
-    AudioQuality,
-    VideoPlatforms,
-    VideoQuality,
-)
+from src.domain.enums import AudioQuality, Platforms
 
 
-class DTO_GetAudioIframe(BaseModel):
-    url: str = Field(min_length=1, max_length=2048)
-    platform: AudioPlatforms = AudioPlatforms.YOUTUBE
+class DTO_GetIframe(BaseModel):
+    url: str = Field(min_length=1, max_length=2083)
+    platform: Platforms = Platforms.YOUTUBE
 
 
-class DTO_GetVideoIframe(BaseModel):
-    url: str = Field(min_length=1, max_length=2048)
-    platform: VideoPlatforms = VideoPlatforms.YOUTUBE
-
-
-class DTO_AudioDownload(BaseModel):
-    url: str = Field(min_length=1, max_length=2048)
+class DTO_Download(BaseModel):
+    url: str = Field(min_length=1, max_length=2083)
     title: Optional[str] = Field(None, min_length=1, max_length=64)
-    platform: AudioPlatforms = AudioPlatforms.YOUTUBE
-    quality: AudioQuality = AudioQuality.MEDIUM
+    platform: Platforms = Platforms.YOUTUBE
 
 
-class DTO_VideoDownload(BaseModel):
-    url: str = Field(min_length=1, max_length=2048)
-    title: Optional[str] = Field(None, min_length=1, max_length=64)
-    platform: VideoPlatforms = VideoPlatforms.YOUTUBE
-    quality: VideoQuality = VideoQuality.MEDIUM
+class DTO_AudioDownload(DTO_Download):
+    # quality: AudioQuality = AudioQuality.HIGH
+    pass
+
+
+class DTO_VideoDownload(DTO_Download):
+    pass
