bc56519e31bde718e60f08d05336388e99aa2d9a
Break <franklinbacacampos@gmail.com>
2026-01-04 22:55:23 -0500

    exponer el content disposition de los headers para permitir leer el nombre del archivo

--- app/main.py
+++ app/main.py
@@ -56,6 +56,7 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["POST"],
     allow_headers=["*"],
+    expose_headers=["Content-Disposition"],
 )
 
 # ------- ENDPOINTS ------------
